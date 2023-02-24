87acf42dcbeaf39261a6cabf14ae36487e23c5ae
Son Jongchan <jcson.pro@gmail.com>
2023-02-24 18:07:05 +0900

    278p

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
+
 
 # Create your tests here.
-# class TestView(TestCase):
-#     def test_post_list(self):
-#         # 1.1 포스트 목록 페이지를 가져온다
-#         # 1.2 정상적으로 페이지가 로드된다
-#         # 1.3 페이지 타이틀은 'Blog'
-#         # 1.4 내비게이션 바가 있다
-#         # 1.5 Blog, About Me라는 문구가 네비
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('노동복지 빅데이터 포털', navbar.text)
+        self.assertIn('Home', navbar.text)
+        self.assertIn('지식정보', navbar.text)
+        self.assertIn('데이터맵', navbar.text)
+        self.assertIn('분석환경', navbar.text)
+        self.assertIn('Log-In', navbar.text)
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.navbar_test(soup)
+
+    def test_post_detail(self):
+        post_000 = Post.objects.create(
+            title= '첫번째 포스트입니다.',
+            content='Hello world',
+        )
+        self.assertEqual(post_000.get_absolute_url(), '/blog/1/')
+        response = self.client.get(post_000.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.navbar_test(soup)
+
