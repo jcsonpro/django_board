30533ee2dd7f8eff8e762a1f0b7f331a7ec49a07
Son Jongchan <jcson.pro@gmail.com>
2023-02-24 17:49:35 +0900

    269p

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+# class TestView(TestCase):
+#     def test_post_list(self):
+#         # 1.1 포스트 목록 페이지를 가져온다
+#         # 1.2 정상적으로 페이지가 로드된다
+#         # 1.3 페이지 타이틀은 'Blog'
+#         # 1.4 내비게이션 바가 있다
+#         # 1.5 Blog, About Me라는 문구가 네비
